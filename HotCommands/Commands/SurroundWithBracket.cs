9ab742893bcaffbb3660021f8525e5ed5eeba648
Pramod Hegde <pramod.hegde@hotmail.com>
2016-07-06 02:35:26 +0530

    Implemented templates based surrounding

--- HotCommands/Commands/SurroundWithBracket.cs
+++ HotCommands/Commands/SurroundWithBracket.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+using HotCommands.Utility;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
@@ -21,6 +27,7 @@ namespace HotCommands
         public static void Initialize(Package package)
         {
             Instance = new SurroundWithBracket(package);
+            TemplateHelper.Init();
         }
 
         private SurroundWithBracket(Package package)
@@ -33,20 +40,53 @@ namespace HotCommands
         public int HandleCommand(IWpfTextView textView, IClassifier classifier, IOleCommandTarget commandTarget, IEditorOperations editorOperations)
         {
             Guid cmdGroup = VSConstants.VSStd2K;
-            int start_position = SelectedPosition_Start(textView);
-            commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.CUT, (uint)OLECMDEXECOPT.OLECMDEXECOPT_PROMPTUSER, IntPtr.Zero, IntPtr.Zero);   // TODO: need show the list of constructs (if, if-else, for, while, do-while etc..)
-            InsertSpan(textView, start_position, "if (condition) {");  // TODO: change this based on user selection
-
-            commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.RETURN, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
-            commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.PASTE, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
-            commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.RETURN, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
-            
-            int end_position = SelectedPosition_End(textView);
-            InsertSpan(textView, end_position, "}");
-
+            EncloseMember(textView, commandTarget, cmdGroup, IsMethodOrProperty(textView));
             editorOperations.Tabify();
-
             return VSConstants.S_OK;
+        }
+
+        private void EncloseMember(IWpfTextView textView, IOleCommandTarget commandTarget, Guid cmdGroup, bool isMethodOrProperty)
+        {
+            MemberTemplate t = isMethodOrProperty ? MemberTemplate.METHOD : MemberTemplate.CLASS;
+            var constructs = t.GetType().GetField(t.ToString()).GetCustomAttribute<TemplateDetailAttribute>().Constructs;
+            IList<Construct> _c = TemplateHelper.ConstructList(constructs);
+
+#pragma warning disable 1587
+            /// TODO: 
+            /// 1. based on 't', show the constructs (by using the list of Construct GUID present in the TemplateDetail)
+            /// 2. show list of constructs & get the user input
+#pragma warning restore 1587
+            commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.CUT, (uint)OLECMDEXECOPT.OLECMDEXECOPT_PROMPTUSER, IntPtr.Zero, IntPtr.Zero);
+
+            string userInput = "7C6B761B-FC42-4F42-93AB-2263A9894986";   /// TODO: remove this later. its just a mock data for now.
+
+            IList<CommandStep> steps = _c.First(x => x.Id.Equals(userInput)).CommandSteps;
+            ProcessCommandSteps(textView, commandTarget, cmdGroup, steps);
+        }
+
+        private void ProcessCommandSteps(IWpfTextView textView, IOleCommandTarget commandTarget, Guid cmdGroup, IList<CommandStep> steps)
+        {
+            foreach (var s in steps)
+            {
+                if (s.StepType == CommandStepType.ADDCONTENT)
+                {
+                    commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.PASTE, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
+                    commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.RETURN, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
+                }
+                else
+                {
+                    if (s.Begin != null)
+                    {
+                        InsertSpan(textView, SelectionStart(textView), s.Begin);
+                        commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.RETURN, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
+                    }
+                    if (s.End != null)
+                    {
+                        InsertSpan(textView, SelectionEnd(textView), s.End);
+                        commandTarget.Exec(ref cmdGroup, (uint)VSConstants.VSStd2KCmdID.RETURN, (uint)OLECMDEXECOPT.OLECMDEXECOPT_DONTPROMPTUSER, IntPtr.Zero, IntPtr.Zero);
+                    }
+                }
+            }
         }
 
         private void InsertSpan(IWpfTextView textView, int position, string text)
@@ -58,8 +98,14 @@ namespace HotCommands
             }
         }
 
+        private bool IsMethodOrProperty(IWpfTextView textView)
+        {
+            return true;
+            /// TODO: implementation pending
+            throw new NotImplementedException();
+        }
 
-        private int SelectedPosition_Start(IWpfTextView textView)
+        private int SelectionStart(IWpfTextView textView)
         {
             foreach (SnapshotSpan snapshotSpan in textView.Selection.SelectedSpans)
             {
@@ -69,7 +115,7 @@ namespace HotCommands
             return 0;
         }
 
-        private int SelectedPosition_End(IWpfTextView textView)
+        private int SelectionEnd(IWpfTextView textView)
         {
             int pos = default(int);
             foreach (SnapshotSpan snapshotSpan in textView.Selection.SelectedSpans)
@@ -78,6 +124,68 @@ namespace HotCommands
                 pos = spanToCheck.End.Position;
             }
             return pos;
+        }
+    }
+
+    internal enum MemberTemplate
+    {
+        [TemplateDetail("7956D06F-EFAC-4C4A-911E-0D26A5AC0CE9", "C571B1BF-EEBC-42CA-A4EA-68541390DC54", "21C8DBAF-B938-41DF-B003-1F8274E70705", "9DF728D6-818D-495B-BDC8-61BEC2346CEA"
+                        , "7849B603-C476-457F-B3EF-11BA188A7141", "3ADAD126-0C7D-4DD7-B320-4CA10D5CABE4", "7C6B761B-FC42-4F42-93AB-2263A9894986")]
+        METHOD = 0,
+
+        [TemplateDetail("11445A9D-FE39-421B-9F72-C85B6C36C454", "DE0CD972-40E5-4F63-BB3A-FDDF48F56471")]
+        CLASS = 1,
+    }
+
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field)]
+    internal sealed class TemplateDetailAttribute : Attribute
+    {
+        internal IList<string> Constructs { get; private set; }
+
+        internal TemplateDetailAttribute(params string[] constructIdentifiers)
+        {
+            Constructs = constructIdentifiers.ToList();
+        }
+    }
+
+    internal sealed class TemplateHelper
+    {
+        private static List<Construct> _c = null;
+
+        private TemplateHelper()
+        {
+        }
+
+        internal static void Init()
+        {
+            if (_c == null)
+                _c = new List<Construct>();
+
+            foreach (var e in GetTemplateStream().Root.Elements())
+                _c.Add(new Construct(e));
+        }
+
+        private static XDocument GetTemplateStream()
+        {
+            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("HotCommands.Resources.SurroundWithCommandTemplate.xml"))
+            {
+                if (stream == null) return null;
+
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    return XDocument.Load(sr.BaseStream);
+                }
+            }
+        }
+
+        internal static IList<Construct> ConstructList(IEnumerable<string> g)
+        {
+            List<Construct> list = new List<Construct>();
+            foreach (var x in _c)
+            {
+                if (g.Contains(x.Id)) list.Add(x);
+            }
+            return list;
         }
     }
 }
---
+++ HotCommands/Utility/0x1023/Construct.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace HotCommands.Utility
+{
+    internal sealed class Construct
+    {
+        internal string DisplayText { get; private set; }
+
+        internal string Id { get; private set; }
+
+        internal IList<CommandStep> CommandSteps { get; private set; }
+
+        private XElement _e = null;
+
+        internal Construct(XElement e)
+        {
+            _e = e;
+            CommandSteps = new List<CommandStep>();
+            Init();
+            LoadSteps();
+        }
+
+        private void Init()
+        {
+            DisplayText = _e.Attributes().First(x => x.Name.LocalName.Equals("display")).Value;
+            Id = _e.Attributes().First(x => x.Name.LocalName.Equals("id")).Value;
+        }
+
+        private void LoadSteps()
+        {
+            foreach (XElement s in _e.Descendants(XName.Get("Step")))
+            {
+                string v = s.Attribute(XName.Get("add")).Value;
+                if (v.Equals("[CONTENT]"))
+                    CommandSteps.Add(new CommandStep { StepType = CommandStepType.ADDCONTENT });
+                else if (v.StartsWith("::"))
+                {
+                    var _x = _e.Descendants(XName.Get("Item")).First(x =>
+                    {
+                        string _a = x.Attribute(XName.Get("index")).Value;
+                        return ("::" + _a).Equals(v);
+                    });
+
+                    CommandSteps.Add(new CommandStep
+                    {
+                        StepType = CommandStepType.ADDCONSTRUCTEXT,
+                        Begin = _x.Attribute(XName.Get("begin")).Value,
+                        End = _x.Attribute(XName.Get("end")).Value
+                    });
+                }
+                else if (v.Equals(":begin"))
+                    CommandSteps.Add(new CommandStep { Begin = _e.Attribute(XName.Get("begin")).Value, StepType = CommandStepType.ADDCONSTRUCT });
+                else
+                    CommandSteps.Add(new CommandStep { End = _e.Attribute(XName.Get("end")).Value, StepType = CommandStepType.ADDCONSTRUCT });
+            }
+        }
+    }
+
+
+    internal sealed class CommandStep
+    {
+        internal string Begin { get; set; }
+
+        internal string End { get; set; }
+
+        internal CommandStepType StepType { get; set; }
+    }
+
+    internal enum CommandStepType
+    {
+        ADDCONSTRUCT,
+        ADDCONSTRUCTEXT,
+        ADDCONTENT
+    }
+}
