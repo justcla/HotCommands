efe2421b0a9332dfd9086753509d8235a6489abd
aakash-1906 <aakash-1906@users.noreply.github.com>
2016-07-20 12:03:50 +0530

    Done with duplicate selection

--- HotCommands/Commands/DuplicateSelection/DuplicateSelection.cs
+++ HotCommands/Commands/DuplicateSelection/DuplicateSelection.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Operations;
@@ -28,7 +31,7 @@ namespace HotCommands.Commands
                 throw new ArgumentNullException(nameof(package));
             _package = package;
         }
-
+        // Helped by source of Microsoft.VisualStudio.Text.Editor.DragDrop.DropHandlerBase.cs in assembly Microsoft.VisualStudio.Text.UI.Wpf, Version=14.0.0.0
         public int HandleCommand(IWpfTextView textView, IClassifier classifier, IOleCommandTarget commandTarget, IEditorOperations editorOperations)
         {
             Guid cmdGroup = VSConstants.VSStd2K;
@@ -40,25 +43,47 @@ namespace HotCommands.Commands
                 editorOperations.SelectLine(textView.Caret.ContainingTextViewLine, false);
                 isSingleLine = true;
             }
-            var selStart = editorOperations.TextView.Caret.ContainingTextViewLine;
-            var selEnd = editorOperations.TextView.Selection.End;
-            editorOperations.CopySelection();
-            // editorOperations.MoveToStartOfLine(false); 
-            editorOperations.MoveToNextCharacter(false);
-            //var startPosition = textView.Caret.Position.VirtualBufferPosition;
-
-            editorOperations.Paste(); 
-
-            editorOperations.SelectAndMoveCaret(textView.Caret.Position.VirtualBufferPosition, textView.Caret.Position.VirtualBufferPosition);
-            editorOperations.MoveToPreviousCharacter(true);
-            editorOperations.MoveCaret(editorOperations.TextView.Caret.ContainingTextViewLine, 0, true);
-
-
 
             if (isSingleLine)
-                editorOperations.MoveToEndOfLine(false);
+            {
+                editorOperations.CopySelection();
+                editorOperations.MoveToNextCharacter(false);
+                editorOperations.Paste();
+                editorOperations.MoveToPreviousCharacter(false);
+            }
+            else
+            {
+                ITextSelection selection = textView.Selection;
+                var virtualBufferPosition = editorOperations.TextView.Caret.Position.VirtualBufferPosition;
+                bool isReversed = selection.IsReversed;
+                string text = selectedText;
+                ITextSnapshot textSnapshot = textView.TextSnapshot;
+                ITrackingPoint trackingPoint = textSnapshot.CreateTrackingPoint(virtualBufferPosition.Position, PointTrackingMode.Negative);
+                List<ITrackingSpan> list = new List<ITrackingSpan>();
+                foreach (SnapshotSpan snapshotSpan in selection.SelectedSpans)
+                    list.Add(textSnapshot.CreateTrackingSpan(snapshotSpan, SpanTrackingMode.EdgeExclusive));
+                if (!selection.IsEmpty)
+                    selection.Clear();
+                int offset = 0;
+                if (virtualBufferPosition.IsInVirtualSpace)
+                {
+                    offset = editorOperations.GetWhitespaceForVirtualSpace(virtualBufferPosition).Length;
+                }
+                textView.Caret.MoveTo(virtualBufferPosition.TranslateTo(textView.TextSnapshot));
+                editorOperations.InsertText(text);
+                SnapshotPoint insertionPoint = trackingPoint.GetPoint(textView.TextSnapshot);
+                if (offset != 0)
+                {
+                    insertionPoint = insertionPoint.Add(offset);
+                }
+                VirtualSnapshotPoint virtualSnapshotPoint1 = new VirtualSnapshotPoint(insertionPoint);
+                VirtualSnapshotPoint virtualSnapshotPoint2 = new VirtualSnapshotPoint(insertionPoint.Add(text.Length));
+                if (isReversed)
+                    editorOperations.SelectAndMoveCaret(virtualSnapshotPoint2, virtualSnapshotPoint1, TextSelectionMode.Stream);
+                else
+                    editorOperations.SelectAndMoveCaret(virtualSnapshotPoint1, virtualSnapshotPoint2, TextSelectionMode.Stream);
+            }
 
-            editorOperations.ResetSelection();
             return VSConstants.S_OK;
         }
     }
