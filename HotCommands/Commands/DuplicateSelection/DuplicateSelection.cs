c4147153c274f930e385d338e40336bdda49dad3
aakash-1906 <aakash-1906@users.noreply.github.com>
2016-07-20 10:25:43 +0530

    Modified logic. Work In progress

--- HotCommands/Commands/DuplicateSelection/DuplicateSelection.cs
+++ HotCommands/Commands/DuplicateSelection/DuplicateSelection.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.Shell;
@@ -17,33 +17,43 @@ namespace HotCommands.Commands
 
         private IServiceProvider ServiceProvider => _package;
 
-        public static void Initialize (Package package)
+        public static void Initialize(Package package)
         {
             Instance = new DuplicateSelection(package);
         }
 
-        private DuplicateSelection (Package package)
+        private DuplicateSelection(Package package)
         {
             if (package == null)
                 throw new ArgumentNullException(nameof(package));
             _package = package;
         }
 
-        public int HandleCommand (IWpfTextView textView, IClassifier classifier, IOleCommandTarget commandTarget, IEditorOperations editorOperations)
+        public int HandleCommand(IWpfTextView textView, IClassifier classifier, IOleCommandTarget commandTarget, IEditorOperations editorOperations)
         {
             Guid cmdGroup = VSConstants.VSStd2K;
             bool isSingleLine = false;
-
-            if (editorOperations.SelectedText.Length == 0)
+            var selectedText = editorOperations.SelectedText;
+            if (selectedText.Length == 0)
             // if nothing is selected, we can consider the current line as a selection
             {
                 editorOperations.SelectLine(textView.Caret.ContainingTextViewLine, false);
                 isSingleLine = true;
             }
-
+            var selStart = editorOperations.TextView.Caret.ContainingTextViewLine;
+            var selEnd = editorOperations.TextView.Selection.End;
             editorOperations.CopySelection();
-            editorOperations.MoveToStartOfLine(false);
-            editorOperations.Paste();
+            // editorOperations.MoveToStartOfLine(false); 
+            editorOperations.MoveToNextCharacter(false);
+            //var startPosition = textView.Caret.Position.VirtualBufferPosition;
+
+            editorOperations.Paste(); 
+
+            editorOperations.SelectAndMoveCaret(textView.Caret.Position.VirtualBufferPosition, textView.Caret.Position.VirtualBufferPosition);
+            editorOperations.MoveToPreviousCharacter(true);
+            editorOperations.MoveCaret(editorOperations.TextView.Caret.ContainingTextViewLine, 0, true);
+
+
 
             if (isSingleLine)
                 editorOperations.MoveToEndOfLine(false);
