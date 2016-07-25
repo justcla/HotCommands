f7ae0017335f799aa808c2917229d7253cf8eef7
Stephen Rodriguez <strodri@Microsoft.com>
2016-07-25 15:34:32 -0700

    Probably implemented move cursor between members functionality.

---
+++ HotCommands/Commands/MoveCursorToPreviousMember.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Text;
+using Microsoft.CodeAnalysis;
+
+namespace HotCommands.Commands
+{
+    class MoveCursorToPreviousMember
+    {
+
+        public int MoveCursorToMember(IWpfTextView textView, bool up)
+        {
+            // Get the Syntax Root
+            var syntaxRoot = textView.TextSnapshot.GetOpenDocumentInCurrentContextWithChanges().GetSyntaxRootAsync().Result;
+
+            // Find the Current Declaration Member from caret Position
+            var currMember = syntaxRoot.FindMemberDeclarationAt(textView.Caret.Position.BufferPosition.Position);
+            if (currMember == null || currMember.Parent == null)
+            {
+                return VSConstants.S_OK;
+            }
+
+            SyntaxNode adjacentMember;
+            if (up)
+            {
+                adjacentMember = syntaxRoot.FindMemberDeclarationAt(currMember.FullSpan.Start - 1);
+            }
+            else
+            {
+                adjacentMember = syntaxRoot.FindMemberDeclarationAt(currMember.FullSpan.End + 1);
+            }
+            if (adjacentMember != null)
+            {
+                // move the cursor to the previous member
+                textView.Caret.MoveTo(new SnapshotPoint(textView.TextSnapshot, adjacentMember.FullSpan.Start));
+            }
+
+            return VSConstants.S_OK;
+        }
+    }
+}
