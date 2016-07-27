d590a3770851248237cec712ed235e7a43dcf40e
Pramod Hegde <pramod.hegde@hotmail.com>
2016-07-27 18:22:38 +0530

    added a portable project

---
+++ ExtractClass.Portable/Actions/ContextEntities/ChangeModifierContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+namespace ExtractClass.Actions
+{
+    internal sealed class ChangeModifierContext : IModifierActionContext
+    {
+        public CodeRefactoringContext Context { get; set; }
+
+        public SyntaxToken NewModifier { get; set; }
+
+        public SyntaxToken OldModifier { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ ExtractClass.Portable/RefactoringProviders/ChangeModifier.cs
@@ -0,0 +1,42 @@
+﻿using ExtractClass.Actions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Composition;
+
+namespace ExtractClass.RefactoringProviders
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(ExtractClassToAFile)), Shared]
+    public class ChangeModifier : CodeRefactoringProvider
+    {
+        public override async Task ComputeRefactoringsAsync (CodeRefactoringContext context)
+        {
+            var rootNode = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var node = rootNode.FindNode(context.Span) as BaseTypeDeclarationSyntax;
+
+            if (node == null || context.Document.Name.ToLowerInvariant() == $"{node.Identifier.ToString().ToLowerInvariant()}.cs") return;
+
+            if (node.Modifiers.Any(SyntaxKind.PrivateKeyword) && !node.IsNested())
+            {
+                context.RegisterRefactoring(new ChangeModifierAction(new ChangeModifierContext
+                {
+                    Context = context,
+                    Title = "To Public",
+                    NewModifier = SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+                    OldModifier = SyntaxFactory.Token(SyntaxKind.PrivateKeyword)
+                }));
+
+                context.RegisterRefactoring(new ChangeModifierAction(new ChangeModifierContext
+                {
+                    Context = context,
+                    Title = "To Internal",
+                    NewModifier = SyntaxFactory.Token(SyntaxKind.InternalKeyword),
+                    OldModifier = SyntaxFactory.Token(SyntaxKind.PrivateKeyword)
+                }));
+            }
+        }
+    }
+}
