9c47eab8b2a71e31888817d24ad4900cefd74a19
Justin Clareburt <justcla@microsoft.com>
2016-08-08 19:45:47 -0700

    Created project: EditorCommands

---
+++ EditorCommands/Commands/FormatCode.cs
@@ -0,0 +1,43 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="FormatCode.cs" company="Microsoft">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Language.StandardClassification;
+using Microsoft.VisualStudio.Text.Operations;
+using OleInterop = Microsoft.VisualStudio.OLE.Interop;
+
+namespace HotCommands
+{
+    /// <summary>
+    /// Command handler for FormatCode
+    /// </summary>
+    internal sealed class FormatCode
+    {
+        public static int HandleCommand(IWpfTextView textView, OleInterop.IOleCommandTarget commandTarget)
+        {
+            Guid cmdGroup = VSConstants.VSStd2K;
+
+            // Execute FormatSelection or FormatDocument depending on current state of selected code
+            uint cmdID = IsCursorOnly(textView) ? (uint)VSConstants.VSStd2KCmdID.FORMATDOCUMENT : (uint)VSConstants.VSStd2KCmdID.FORMATSELECTION;
+            commandTarget.Exec(ref cmdGroup, cmdID, (uint)OleInterop.OLECMDEXECOPT.OLECMDEXECOPT_DODEFAULT, IntPtr.Zero, IntPtr.Zero);
+
+            return VSConstants.S_OK;
+        }
+
+        private static bool IsCursorOnly(IWpfTextView textView)
+        {
+            if (textView.Selection.SelectedSpans.Count > 1) return false;
+            // Only one selection. Check if there is any selected content.
+            return textView.Selection.SelectedSpans[0].Length == 0;
+        }
+    }
+}
