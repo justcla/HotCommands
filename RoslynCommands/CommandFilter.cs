a9aac19d340fbeb3222d5b89fff39ac43bfdefe9
Justin Clareburt <justcla@microsoft.com>
2016-08-11 18:19:55 -0700

    Attempting RoslynCommands project

---
+++ RoslynCommands/CommandFilter.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using OLEConstants = Microsoft.VisualStudio.OLE.Interop.Constants;
+using System;
+using HotCommands.Commands;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text.Operations;
+
+namespace HotCommands
+{
+    internal sealed class CommandFilter : IOleCommandTarget
+    {
+        private readonly IWpfTextView textView;
+        private readonly IClassifier classifier;
+        private readonly SVsServiceProvider globalServiceProvider;
+        private IEditorOperations editorOperations;
+
+        public CommandFilter(IWpfTextView textView, IClassifierAggregatorService aggregatorFactory,
+            SVsServiceProvider globalServiceProvider, IEditorOperationsFactoryService editorOperationsFactory)
+        {
+            this.textView = textView;
+            classifier = aggregatorFactory.GetClassifier(textView.TextBuffer);
+            this.globalServiceProvider = globalServiceProvider;
+            editorOperations = editorOperationsFactory.GetEditorOperations(textView);
+        }
+
+        public IOleCommandTarget Next { get; internal set; }
+
+        public int Exec(ref Guid pguidCmdGroup, uint nCmdID, uint nCmdexecopt, IntPtr pvaIn, IntPtr pvaOut)
+        {
+            // Command handling
+            if (pguidCmdGroup == Constants.HotCommandsGuid)
+            {
+                // Dispatch to the correct command handler
+                switch (nCmdID)
+                {
+                    case Constants.ExpandSelectionCmdId:
+                        return ExpandSelection.Instance.HandleCommand(textView, true);
+                    case Constants.ShrinkSelectionCmdId:
+                        return ExpandSelection.Instance.HandleCommand(textView, false);
+                    case Constants.MoveMemberUpCmdId:
+                        return MoveMemberUp.Instance.HandleCommand(textView,GetShellCommandDispatcher(), editorOperations);
+                    case Constants.MoveMemberDownCmdId:
+                        return MoveMemberDown.Instance.HandleCommand(textView,GetShellCommandDispatcher(), editorOperations);
+                    case Constants.MoveCursorPrevMember:
+                        return MoveCursorToAdjacentMember.MoveToPreviousMember(textView, editorOperations);
+                    case Constants.MoveCursorNextMember:
+                        return MoveCursorToAdjacentMember.MoveToNextMember(textView, editorOperations);
+                }
+            }
+
+            // No commands called. Pass to next command handler.
+            if (Next != null)
+            {
+                return Next.Exec(ref pguidCmdGroup, nCmdID, nCmdexecopt, pvaIn, pvaOut);
+            }
+            return (int)OLEConstants.OLECMDERR_E_UNKNOWNGROUP;
+        }
+
+        public int QueryStatus(ref Guid pguidCmdGroup, uint cCmds, OLECMD[] prgCmds, IntPtr pCmdText)
+        {
+            // Command handling registration
+            if (pguidCmdGroup == Constants.HotCommandsGuid && cCmds == 1)
+            {
+                switch (prgCmds[0].cmdID)
+                {
+                    case Constants.ExpandSelectionCmdId:
+                    case Constants.MoveMemberUpCmdId:
+                    case Constants.MoveMemberDownCmdId:
+                    case Constants.MoveCursorPrevMember:
+                    case Constants.MoveCursorNextMember:
+                        prgCmds[0].cmdf |= (uint)OLECMDF.OLECMDF_ENABLED;
+                        return VSConstants.S_OK;
+                }
+            }
+
+            if (Next != null)
+            {
+                return Next.QueryStatus(ref pguidCmdGroup, cCmds, prgCmds, pCmdText);
+            }
+            return (int)OLEConstants.OLECMDERR_E_UNKNOWNGROUP;
+        }
+
+        /// <summary>
+        /// Get the SUIHostCommandDispatcher from the global service provider.
+        /// </summary>
+        private IOleCommandTarget GetShellCommandDispatcher()
+        {
+            return globalServiceProvider.GetService(typeof(SUIHostCommandDispatcher)) as IOleCommandTarget;
+        }
+    }
+}
---
+++ RoslynCommands/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HotCommands
+{
+    public class Constants
+    {
+        public static readonly Guid HotCommandsGuid = new Guid("1023dc3d-550c-46b8-a3ec-c6b03431642c");
+        public const uint ExpandSelectionCmdId = 0x1022;
+        public const uint ShrinkSelectionCmdId = 0x1023;
+        public const uint MoveMemberUpCmdId = 0x1031;
+        public const uint MoveMemberDownCmdId = 0x1032;
+        public const uint MoveCursorPrevMember = 0x1033;
+        public const uint MoveCursorNextMember = 0x1034;
+    }
+}
