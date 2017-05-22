956c8aa40ae0d7309449c694d1bc8241c12ba93c
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-05-22 22:01:02 +0200

    Add required deps, and set correct target .NET
    Framework version.

---
+++ HotRefactorings.Tests/ChangeModifierTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotCommands;
+using Xunit;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+
+namespace HotRefactorings.Tests
+{
+    public class ChangeModifierTests
+    {
+        [Fact]
+        public async Task PublicToInternalTestsAsync()
+        {
+            var context = new CodeRefactoringContext();
+            var sut = new ChangeModifier();
+
+            await sut.ComputeRefactoringsAsync(context);
+        }
+    }
+}
