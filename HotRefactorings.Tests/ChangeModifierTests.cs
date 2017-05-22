1eb315a0cd9895201307b870256acd974a1bfbff
Thijs Brobbel <t.brobbel@coolblue.nl>
2017-05-22 23:10:12 +0200

    Cover all scenario's from public to *.

--- HotRefactorings.Tests/ChangeModifierTests.cs
+++ HotRefactorings.Tests/ChangeModifierTests.cs
@@ -8,6 +8,21 @@ namespace HotRefactorings.Tests
     public class ChangeModifierTests : CodeRefactoringVerifier
     {
         [Fact]
+        public void PublicToProtectedTests()
+        {
+            var oldSource =
+@"public class Class1
+{
+}";
+
+            var newSource =
+@"protected class Class1
+{
+}";
+            VerifyRefactoring(oldSource, newSource, 0, "To Protected");
+        }
+
+        [Fact]
         public void PublicToInternalTests()
         {
             var oldSource =
@@ -22,6 +37,35 @@ namespace HotRefactorings.Tests
             VerifyRefactoring(oldSource, newSource, 0, "To Internal");
         }
 
+        [Fact]
+        public void PublicToPrivateTests()
+        {
+            var oldSource =
+@"public class Class1
+{
+}";
+
+            var newSource =
+@"private class Class1
+{
+}";
+            VerifyRefactoring(oldSource, newSource, 0, "To Private");
+        }
+
+        [Fact]
+        public void PublicToProtectedInternalTests()
+        {
+            var oldSource =
+@"public class Class1
+{
+}";
+
+            var newSource =
+@"protected internal class Class1
+{
+}";
+            VerifyRefactoring(oldSource, newSource, 0, "To Protected Internal");
+        }
         protected override CodeRefactoringProvider GetCodeRefactoringProvider()
         {
             return new ChangeModifier();
