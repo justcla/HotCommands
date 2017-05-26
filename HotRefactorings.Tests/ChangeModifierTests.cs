fa0ff4c205f90f237f34095d1e6f14af0bd3feca
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-26 21:58:09 +0200

    Add unit tests for change modifier refactoring (#68)
    * Add unit test project for HotRefactorings.
    
    * Ignore NCrunch config files.
    
    * Remove template generated unit test.
    
    * Add required deps, and set correct target .NET
    
    Framework version.
    
    * Add CodeRefactoringVerifier based on the
    
    {Diagnostic,CodeFix}Verifier from the Analyzer + CodeFix project
    templates.
    
    * Cover all scenario's from public to *.
    
    * Convert separate facts to theories,
    
    in order to simplify the tests.
    
    * Add some more additional tests for missing scenarios,
    
    like a test for 'protected internal' -> 'protected' only.
    
    * Reference NuGet package M.C.CSharp.Workspaces,
    
    since otherwise the tests won't run in VS Test Explorer.

---
+++ HotRefactorings.Tests/ChangeModifierTests.cs
@@ -0,0 +1,67 @@
+﻿using HotCommands;
+using Xunit;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using TestHelper;
+
+namespace HotRefactorings.Tests
+{
+    public class ChangeModifierTests : CodeRefactoringVerifier
+    {
+        [Theory]
+        [InlineData("public", "protected", "To Protected")]
+        [InlineData("public", "internal", "To Internal")]
+        [InlineData("public", "private", "To Private")]
+        [InlineData("public", "protected internal", "To Protected Internal")]
+        public void FromToModifierTheory(string fromModifier, string toModifier, string refactoringTitle)
+        {
+            var oldSource = CreateClassWithModifier(fromModifier);
+            var newSource = CreateClassWithModifier(toModifier);
+
+            VerifyRefactoring(oldSource, newSource, 0, refactoringTitle);
+        }
+
+        [Fact]
+        public void ProtectedInternalToPublicTest()
+        {
+            var oldSource = CreateClassWithModifier("protected internal");
+            var newSource = CreateClassWithModifier("public");
+            VerifyRefactoring(oldSource, newSource, 0, "To Public");
+        }
+
+        [Fact]
+        public void ProtectedInternalToProtectedOnlyTest()
+        {
+            var oldSource = CreateClassWithModifier("protected internal");
+            var newSource = CreateClassWithModifier("protected");
+            VerifyRefactoring(oldSource, newSource, 0, "To Protected (only)");
+        }
+
+        [Fact]
+        public void ToPublicFromRedundantModifiersTest()
+        {
+            var oldSource = CreateClassWithModifier("public private");
+            var newSource = CreateClassWithModifier("public");
+            VerifyRefactoring(oldSource, newSource, 0, "To Public (Remove redundant modifiers)");
+        }
+
+        [Fact]
+        public void PublicStaticClassToInternalShouldKeepStatic()
+        {
+            var oldSource = CreateClassWithModifier("public static");
+            var newSource = CreateClassWithModifier("internal static");
+            VerifyRefactoring(oldSource, newSource, 0, "To Internal");
+        }
+
+        private static string CreateClassWithModifier(string modifier)
+        {
+            return $@"{modifier} class Class1
+{{
+}}";
+        }
+
+        protected override CodeRefactoringProvider GetCodeRefactoringProvider()
+        {
+            return new ChangeModifier();
+        }
+    }
+}
